0406af174a8f715ef732f140de8f0d4e56dac22d
Felix <femaury@student.42.fr>
2019-08-27 20:24:18 +0200

    Removed first and last name user search as it has been removed from API...

--- Intra42/APIManager/Extensions/Users.swift
+++ Intra42/APIManager/Extensions/Users.swift
@@ -11,23 +11,24 @@ import SwiftyJSON
 
 extension API42Manager {
     // TODO: Get application API token officialized to make more than 2 requests per second.
+    // first_name and last_name have been removed from search parameter (27/08/19)
     /**
      Gets all users (up to 3 x 100) whose login, first name or last name matches the string.
      */
     func searchUsers(withString string: String, completionHander: @escaping (JSON?, SearchSection) -> Void) {
         let loginURL = "https://api.intra.42.fr/v2/users?search[login]=\(string)&sort=login&page[size]=100"
-        let firstNameURL = "https://api.intra.42.fr/v2/users?search[first_name]=\(string)&sort=login&page[size]=100"
-        let lastNameURL = "https://api.intra.42.fr/v2/users?search[last_name]=\(string)&sort=login&page[size]=100"
+//        let firstNameURL = "https://api.intra.42.fr/v2/users?search[first_name]=\(string)&sort=login&page[size]=100"
+//        let lastNameURL = "https://api.intra.42.fr/v2/users?search[last_name]=\(string)&sort=login&page[size]=100"
 
         request(url: loginURL) { (responseJSON) in
             completionHander(responseJSON, .username)
         }
-        request(url: firstNameURL) { (responseJSON) in
-            completionHander(responseJSON, .firstName)
-
-            self.request(url: lastNameURL) { (responseJSON) in
-                completionHander(responseJSON, .lastName)
-            }
-        }
+//        request(url: firstNameURL) { (responseJSON) in
+//            completionHander(responseJSON, .firstName)
+//
+//            self.request(url: lastNameURL) { (responseJSON) in
+//                completionHander(responseJSON, .lastName)
+//            }
+//        }
     }
 }
